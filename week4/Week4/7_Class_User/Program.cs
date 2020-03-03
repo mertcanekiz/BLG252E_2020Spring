3d59b3ad40d0a1f25f3d9f5f4f9599e28477474e
mertcanekiz <ekiz.mertcan@gmail.com>
2020-03-03 07:19:50 +0300

    Week 4 Examples

---
+++ week4/Week4/7_Class_User/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7_Class_User
+{
+    class Program
+    {
+        class User
+        {
+            public string email;
+            public string password;
+
+            public User(string email, string password)
+            {
+                this.email = email;
+                this.password = password;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"{email}: {password}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+                users.Add(new User(email, password));
+            }
+
+            foreach (User user in users)
+            {
+                user.Print();
+            }
+
+            users.RemoveAt(0);
+        }
+    }
+}
---
+++ week4/Week4/Class/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Class
+{
+    class Rectangle
+    {
+        public double width;
+        public double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Area()
+        {
+            return width * height;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Width: {width} Height: {height} Area: {Area()}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle(10, 5);
+            rect1.Print();
+
+            Rectangle rect2 = new Rectangle(20, 20);
+            rect2.Print();
+        }
+    }
+}
